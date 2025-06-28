5f6f53245fde13fb584b4b7473c8a10d77e779b0
joao01sb <bonfimnew@gmail.com>
2025-06-28 20:20:45 -0300

    feat: Update Kotlin and add libraries
    Updated Kotlin to version 2.1.21 and Compose BOM to 2025.06.01.
    Added dependencies and plugins for:
    - Navigation Compose
    - Hilt
    - Detekt
    - Kotlinx Kover

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android.gradle)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlinx.kover)
 }
 
 android {
@@ -49,11 +53,23 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Navigation Compose
+    implementation(libs.androidx.navigation.compose)
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    //Detekt
+    detektPlugins(libs.detekt.formatting)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.hilt.android.gradle) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.kotlinx.kover) apply false
 }
