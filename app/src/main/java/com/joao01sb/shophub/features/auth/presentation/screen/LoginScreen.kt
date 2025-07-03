1d9b6ecb5f4a597e33d9925ba36e23959ac64c18
joao01sb <bonfimnew@gmail.com>
2025-07-02 21:56:33 -0300

    feat: Implement Login and Register screens
    Created `LoginScreen.kt` and `RegisterScreen.kt` with Jetpack Compose.
    - Implemented UI elements for email, password, and username input.
    - Added buttons for login and registration actions.
    - Included keyboard actions for better user experience.
    - Added previews for both screens.
    
    Configured project with App Insights, VCS mappings, inspection profiles, and deployment target selector.

---
+++ app/src/main/java/com/joao01sb/shophub/features/auth/presentation/screen/LoginScreen.kt
@@ -0,0 +1,163 @@
+package com.joao01sb.shophub.features.auth.presentation.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.joao01sb.shophub.R
+import com.joao01sb.shophub.features.auth.presentation.state.AuthUiState
+
+@Composable
+fun LoginScreen(
+    uiState: AuthUiState = AuthUiState(),
+    onValueEmail: (String) -> Unit = {},
+    onValuePassword: (String) -> Unit = {},
+    onClickRegisterNavigation: () -> Unit = {},
+    onClickLogin: () -> Unit = {}
+) {
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            modifier = Modifier.size(180.dp),
+            contentScale = ContentScale.Crop,
+            contentDescription = "ShopHub Logo",
+        )
+
+        Text(
+            text = "ShopHub",
+            style = MaterialTheme.typography.headlineLarge,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.weight(0.2f))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = uiState.email,
+                onValueChange = onValueEmail,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email icon"
+                    )
+                },
+                label = { Text(text = "Email") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                )
+            )
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = uiState.password,
+                onValueChange = onValuePassword,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password icon"
+                    )
+                },
+                label = { Text(text = "Password") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                        onClickLogin()
+                    }
+                )
+            )
+
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onClickLogin,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text(
+                    text = "Login",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+
+            OutlinedButton(
+                onClick = onClickRegisterNavigation,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Register",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.weight(0.7f))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    MaterialTheme {
+        LoginScreen()
+    }
+}
---
+++ app/src/main/java/com/joao01sb/shophub/features/auth/presentation/screen/RegisterScreen.kt
@@ -0,0 +1,168 @@
+package com.joao01sb.shophub.features.auth.presentation.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.joao01sb.shophub.features.auth.presentation.state.AuthUiState
+
+@Composable
+fun RegisterScreen(
+    uiState: AuthUiState = AuthUiState(),
+    onValueName: (String) -> Unit = {},
+    onValueEmail: (String) -> Unit = {},
+    onValuePassword: (String) -> Unit = {},
+    onClickRegister: () -> Unit = {}
+) {
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(modifier = Modifier.weight(0.2f))
+
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+                .testTag("title_register"),
+            text = "Enter the data below for registration",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.W600,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+
+        Spacer(modifier = Modifier.weight(0.1f))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = uiState.name,
+                onValueChange = onValueName,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "Username icon"
+                    )
+                },
+                label = { Text(text = "Username") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                )
+            )
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = uiState.email,
+                onValueChange = onValueEmail,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email icon"
+                    )
+                },
+                label = { Text(text = "Email") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                )
+            )
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = uiState.password,
+                onValueChange = onValuePassword,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password icon"
+                    )
+                },
+                label = { Text(text = "Password") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                        onClickRegister()
+                    }
+                )
+            )
+
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onClickRegister,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text(
+                    text = "Register",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.weight(0.7f))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    MaterialTheme {
+        RegisterScreen()
+    }
+}
