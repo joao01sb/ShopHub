ed56cf7d16a3325da4d7b187008065c75690e694
joao01sb <bonfimnew@gmail.com>
2025-07-05 17:18:35 -0300

    feat: Generalize PaginatedResponse to support any type
    Updated `PaginatedProductsResponse` to `PaginatedResponse<T>` to allow for pagination of various data types, not just `ProductDto`.

--- app/src/main/java/com/joao01sb/shophub/core/data/remote/dto/PaginatedProductsResponse.kt
+++ app/src/main/java/com/joao01sb/shophub/core/data/remote/dto/PaginatedProductsResponse.kt
@@ -3,8 +3,8 @@ package com.joao01sb.shophub.core.data.remote.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PaginatedProductsResponse(
-    val products: List<ProductDto>,
+data class PaginatedResponse<T>(
+    val products: List<T>,
     val total: Int,
     val skip: Int,
     val limit: Int
