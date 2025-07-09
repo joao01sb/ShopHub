3184b45ccbc60175dc136c218101a5b72aa6e795
joao01sb <bonfimnew@gmail.com>
2025-07-09 19:46:21 -0300

    feat: Define SearchState enum and update SearchUiState
    Created `SearchState.kt` enum to represent different states of the search feature:
    - `RECENT_SEARCHES`
    - `SEARCHING`
    - `RESULTS`
    - `EMPTY_RESULTS`
    - `ERROR`

---
+++ app/src/main/java/com/joao01sb/shophub/features/home/domain/enum/SearchState.kt
@@ -0,0 +1,9 @@
+package com.joao01sb.shophub.features.home.domain.enum
+
+enum class SearchState {
+    RECENT_SEARCHES,
+    SEARCHING,
+    RESULTS,
+    EMPTY_RESULTS,
+    ERROR
+}
--- app/src/main/java/com/joao01sb/shophub/features/home/presentation/state/SearchUiState.kt
+++ app/src/main/java/com/joao01sb/shophub/features/home/presentation/state/SearchUiState.kt
@@ -1,6 +1,12 @@
 package com.joao01sb.shophub.features.home.presentation.state
 
+import com.joao01sb.shophub.core.domain.model.Product
+import com.joao01sb.shophub.features.home.domain.enum.SearchState
+
 data class SearchUiState(
+    val searchQuery: String = "",
+    val recentSearches: List<String> = emptyList(),
     val isSearching: Boolean = false,
+    val searchState: SearchState = SearchState.RECENT_SEARCHES,
     val error: String? = null
 )
