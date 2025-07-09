383bf815e149fa0422844d22e27b202d3a93af70
joao01sb <bonfimnew@gmail.com>
2025-07-08 23:05:11 -0300

    feat: Create SearchProductScreen

---
+++ app/src/main/java/com/joao01sb/shophub/features/home/presentation/screen/SearchProductScreen.kt
@@ -0,0 +1,11 @@
+package com.joao01sb.shophub.features.home.presentation.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchProductScreen(
+    onClickProduct: (Int) -> Unit,
+
+) {
+
+}
