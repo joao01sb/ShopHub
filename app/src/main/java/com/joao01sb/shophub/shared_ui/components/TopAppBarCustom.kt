a76a663bda1881046714f4afc0d6e385034ef88d
joao01sb <bonfimnew@gmail.com>
2025-07-13 12:06:53 -0300

    refactor: Remove unused imports in TopAppBarCustom
    Removed unused imports from `TopAppBarCustom.kt`:
    - `androidx.compose.material.icons.automirrored.filled.ArrowBack`
    - `androidx.compose.material3.Divider`
    - `androidx.compose.material3.IconButton`
    - `androidx.compose.material3.MaterialTheme`
    - `androidx.compose.material3.TopAppBar`

--- app/src/main/java/com/joao01sb/shophub/shared_ui/components/TopAppBarCustom.kt
+++ app/src/main/java/com/joao01sb/shophub/shared_ui/components/TopAppBarCustom.kt
@@ -12,15 +12,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
