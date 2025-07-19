e27701384475fa324080cdc80e1fd8bd7a8daa5f
joao01sb <bonfimnew@gmail.com>
2025-07-19 19:07:18 -0300

    feat: Implement CheckoutScreen with navigation and event handling

--- app/src/main/java/com/joao01sb/shophub/core/navigation/graphs/CartGraph.kt
+++ app/src/main/java/com/joao01sb/shophub/core/navigation/graphs/CartGraph.kt
@@ -1,7 +1,11 @@
 package com.joao01sb.shophub.core.navigation.graphs
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
@@ -9,11 +13,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.joao01sb.shophub.core.navigation.Routes
-import com.joao01sb.shophub.features.auth.presentation.event.AuthUiEvent
 import com.joao01sb.shophub.features.cart.presentation.event.CartEvent
+import com.joao01sb.shophub.features.cart.presentation.event.CheckoutEvent
 import com.joao01sb.shophub.features.cart.presentation.screen.CartScreen
+import com.joao01sb.shophub.features.cart.presentation.screen.CheckoutScreen
 import com.joao01sb.shophub.features.cart.presentation.state.CartUiEvent
+import com.joao01sb.shophub.features.cart.presentation.state.CheckoutUiEvent
 import com.joao01sb.shophub.features.cart.presentation.viewmodel.CartViewModel
+import com.joao01sb.shophub.features.cart.presentation.viewmodel.CheckoutViewModel
 
 fun NavGraphBuilder.cartGraph(
     navController: NavHostController,
@@ -30,9 +37,7 @@ fun NavGraphBuilder.cartGraph(
                 viewModel.uiEvent.collect { event ->
                     when (event) {
                         CartUiEvent.Back -> {
-                            navController.navigate(Routes.HomeGraph) {
-                                popUpTo(Routes.AuthGraph) { inclusive = true }
-                            }
+                            navController.navigate(Routes.HomeGraph)
                         }
                         CartUiEvent.Checkout -> {
                             navController.navigate(Routes.Checkout)
@@ -61,6 +66,55 @@ fun NavGraphBuilder.cartGraph(
             )
 
         }
-        composable<Routes.Checkout> {  }
+        composable<Routes.Checkout> {
+            val viewModel = hiltViewModel<CheckoutViewModel>()
+            val uiState by viewModel.checkoutSate.collectAsStateWithLifecycle()
+            val currentContex = LocalContext.current
+
+            LaunchedEffect(Unit) {
+                viewModel.checkoutUiEvent.collect { event ->
+                    when (event) {
+                        is CheckoutUiEvent.Back -> {
+                            navController.navigate(Routes.Cart)
+                        }
+                        is CheckoutUiEvent.Error -> {
+                            Toast.makeText(currentContex, event.message, Toast.LENGTH_LONG).show()
+                        }
+                        is CheckoutUiEvent.Finaly -> {
+                            Toast.makeText(currentContex, "Success", Toast.LENGTH_LONG).show()
+                            navController.navigate(Routes.HomeGraph) {
+                                popUpTo(Routes.AuthGraph) { inclusive = true }
+                            }
+                        }
+                    }
+                }
+            }
+
+            CheckoutScreen(
+                modifier = Modifier.fillMaxSize(),
+                uiState = uiState,
+                onPlaceOrder = {
+                    viewModel.onEvent(CheckoutEvent.Checkout)
+                },
+                onChangedCvv = {
+                    viewModel.onEvent(CheckoutEvent.CardCVVChanged(it))
+                },
+                onChangedCardNumber = {
+                    viewModel.onEvent(CheckoutEvent.CardNumberChanged(it))
+                },
+                onChangedExpirationCard = {
+                    viewModel.onEvent(CheckoutEvent.CardValidationChanged(it))
+                },
+                onChangedCardHolderName = {
+                    viewModel.onEvent(CheckoutEvent.CardNameChanged(it))
+                },
+                onChangedFullName = {
+                    viewModel.onEvent(CheckoutEvent.FullNameChanged(it))
+                },
+                onChangedPhone = {
+                    viewModel.onEvent(CheckoutEvent.PhoneChanged(it))
+                }
+            )
+        }
     }
 }
