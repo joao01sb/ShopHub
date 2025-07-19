df3524e71ec0f1eca900968b2f8419b93c698e53
joao01sb <bonfimnew@gmail.com>
2025-07-19 19:05:59 -0300

    feat: Refactor ValidateCheckoutInfoUseCase to accept CheckoutInfo as a parameter

--- app/src/main/java/com/joao01sb/shophub/features/cart/domain/usecase/ValidateCheckoutInfoUseCase.kt
+++ app/src/main/java/com/joao01sb/shophub/features/cart/domain/usecase/ValidateCheckoutInfoUseCase.kt
@@ -2,11 +2,9 @@ package com.joao01sb.shophub.features.cart.domain.usecase
 
 import com.joao01sb.shophub.features.cart.domain.model.CheckoutInfo
 
-class ValidateCheckoutInfoUseCase(
-    val checkoutInfo: CheckoutInfo
-) {
+class ValidateCheckoutInfoUseCase {
 
-    operator fun invoke(): Boolean {
+    operator fun invoke(checkoutInfo: CheckoutInfo): Boolean {
         return checkoutInfo.numberCard.isNotEmpty() &&
                 checkoutInfo.nameCard.isNotEmpty() &&
                 checkoutInfo.dateCard.isNotEmpty() &&
