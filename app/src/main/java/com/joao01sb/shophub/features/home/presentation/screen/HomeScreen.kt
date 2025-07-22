0541bb75a8b05c960170a5a7756e7e6cafb00245
joao01sb <bonfimnew@gmail.com>
2025-07-21 22:55:44 -0300

    feat: Enhance loading indicator in HomeScreen with centered layout

--- app/src/main/java/com/joao01sb/shophub/features/home/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/joao01sb/shophub/features/home/presentation/screen/HomeScreen.kt
@@ -1,13 +1,17 @@
 package com.joao01sb.shophub.features.home.presentation.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.joao01sb.shophub.core.domain.model.Product
@@ -27,8 +31,7 @@ fun HomeScreen(
     ) {
         SearchHeader(
             modifier = Modifier.fillMaxWidth(),
-            onSearchClick = onSearchClick,
-            onCartClick = onCartClick
+            onSearchClick = onSearchClick
         )
         LazyVerticalGrid(
             modifier = Modifier.weight(1f),
@@ -44,7 +47,14 @@ fun HomeScreen(
             }
             item {
                 if (products.loadState.append is LoadState.Loading) {
-                    CircularProgressIndicator()
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
+                    ){
+                        CircularProgressIndicator()
+                    }
                 }
             }
 
