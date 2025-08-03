9af7c01b4b065b5024a2a9f6d9beb006d41c264d
joao01sb <bonfimnew@gmail.com>
2025-08-03 12:13:52 -0300

    feat: Add validation for checkout information

--- app/src/main/java/com/joao01sb/shophub/features/cart/domain/usecase/ValidateCheckoutInfoUseCase.kt
+++ app/src/main/java/com/joao01sb/shophub/features/cart/domain/usecase/ValidateCheckoutInfoUseCase.kt
@@ -5,11 +5,13 @@ import com.joao01sb.shophub.features.cart.domain.model.CheckoutInfo
 class ValidateCheckoutInfoUseCase {
 
     operator fun invoke(checkoutInfo: CheckoutInfo): Boolean {
-        return checkoutInfo.numberCard.isNotEmpty() &&
+        return checkoutInfo.numberCard.length == 16 &&
                 checkoutInfo.nameCard.isNotEmpty() &&
-                checkoutInfo.dateCard.isNotEmpty() &&
-                checkoutInfo.cvvCard.isNotEmpty() &&
-                checkoutInfo.fullName.isNotEmpty()
+                checkoutInfo.dateCard.length == 4 &&
+                checkoutInfo.cvvCard.length >= 3 &&
+                checkoutInfo.fullName.isNotEmpty() &&
+                checkoutInfo.phoneNumber.length >= 10
     }
+
 
 }
