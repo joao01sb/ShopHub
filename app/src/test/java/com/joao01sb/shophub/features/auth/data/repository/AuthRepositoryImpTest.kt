d01fe5b66479e61fd4240e629bfe6e0cf213ba4e
joao01sb <bonfimnew@gmail.com>
2025-08-05 23:56:24 -0300

    feat: Add initial structure for AuthRepositoryImpTest
    Created `AuthRepositoryImpTest.kt` with a basic class structure and a `setUp` method.

---
+++ app/src/test/java/com/joao01sb/shophub/features/auth/data/repository/AuthRepositoryImpTest.kt
@@ -0,0 +1,11 @@
+package com.joao01sb.shophub.features.auth.data.repository
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class AuthRepositoryImpTest {
+    @Before
+    fun setUp() {
+    }
+
+}
