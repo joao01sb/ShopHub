045599bb92f00d0360343e34d04f242a3d81a571
joao01sb <bonfimnew@gmail.com>
2025-08-08 15:37:18 -0300

    feat: Add extension function to transform DomainResult success data

--- app/src/main/java/com/joao01sb/shophub/core/result/DomainResult.kt
+++ app/src/main/java/com/joao01sb/shophub/core/result/DomainResult.kt
@@ -6,3 +6,10 @@ sealed class DomainResult<out T> {
     data class Success<T>(val data: T) : DomainResult<T>()
     data class Error(val message: String, val type: ErrorType) : DomainResult<Nothing>()
 }
+
+inline fun <T, R> DomainResult<T>.toDomainResult(transform: (T) -> R): DomainResult<R> {
+    return when (this) {
+        is DomainResult.Success -> DomainResult.Success(transform(this.data))
+        is DomainResult.Error -> this
+    }
+}
