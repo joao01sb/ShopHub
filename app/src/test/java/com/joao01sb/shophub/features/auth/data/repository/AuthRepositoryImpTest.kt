f8bb92f68c8b598a178ae2a3824913339eb28358
joao01sb <bonfimnew@gmail.com>
2025-08-23 14:55:06 -0300

    test: Add unit tests for login, logout, and user retrieval scenarios

--- app/src/test/java/com/joao01sb/shophub/features/auth/data/repository/AuthRepositoryImpTest.kt
+++ app/src/test/java/com/joao01sb/shophub/features/auth/data/repository/AuthRepositoryImpTest.kt
@@ -93,4 +93,204 @@ class AuthRepositoryImpTest {
 
     }
 
+    @Test
+    fun givenInvalidCredentials_whenLogin_thenFailure() = runTest {
+        val email = "test@email.com"
+        val password = "password123"
+        val errorMessage = "Invalid credentials"
+
+        coEvery { authRemoteDataSource.login(email, password) } returns
+                FirebaseResult.UnknownError(Exception(errorMessage))
+
+        val result = authRemoteDataSource.login(email, password)
+
+        assert(result is FirebaseResult.UnknownError)
+
+        assert((result as FirebaseResult.UnknownError).exception.message == errorMessage)
+
+        coVerify(exactly = 1) { authRemoteDataSource.login(email, password) }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenLogin_thenNetworkError() = runTest {
+        val email = "test@email.com"
+        val password = "password123"
+        val errorMessage = "Network error"
+        val codeError = "500"
+
+        coEvery { authRemoteDataSource.login(email, password) } returns
+                FirebaseResult.FirebaseError(codeError, errorMessage)
+
+        val result = authRemoteDataSource.login(email, password)
+
+        assert(result is FirebaseResult.FirebaseError)
+
+        assert((result as FirebaseResult.FirebaseError).message == errorMessage)
+
+        coVerify(exactly = 1) { authRemoteDataSource.login(email, password) }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenSaveUser_thenSuccess() = runTest {
+        val uid = "Uid123"
+        val email = "test@email.com"
+        val name = "Test User"
+
+        coEvery { authRemoteDataSource.saveUser(uid, email, name) } returns FirebaseResult.Success(Unit)
+
+        val result = authRemoteDataSource.saveUser(uid, email, name)
+
+        assert(result is FirebaseResult.Success)
+
+        coVerify(exactly = 1) { authRemoteDataSource.saveUser(uid, email, name) }
+
+    }
+
+    @Test
+    fun givenInvalidCredentials_whenSaveUser_thenFailure() = runTest {
+        val uid = "Uid123"
+        val email = "test@email.com"
+
+        coEvery { authRemoteDataSource.saveUser(uid, email, any()) } returns
+                FirebaseResult.UnknownError(Exception("Invalid credentials"))
+
+        val result = authRemoteDataSource.saveUser(uid, email, "Test User")
+
+        assert(result is FirebaseResult.UnknownError)
+
+        assert((result as FirebaseResult.UnknownError).exception.message == "Invalid credentials")
+
+        coVerify(exactly = 1) { authRemoteDataSource.saveUser(uid, email, "Test User") }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenSaveUser_thenNetworkError() = runTest {
+        val uid = "Uid123"
+        val email = "test@email.com"
+
+        coEvery {
+            authRemoteDataSource.saveUser(
+                uid,
+                email,
+                any()
+            )
+        } returns FirebaseResult.FirebaseError("500", "Network error")
+
+        val result = authRemoteDataSource.saveUser(uid, email, "Test User")
+
+        assert(result is FirebaseResult.FirebaseError)
+
+        assert((result as FirebaseResult.FirebaseError).message == "Network error")
+
+        coVerify(exactly = 1) { authRemoteDataSource.saveUser(uid, email, "Test User") }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenLogout_thenSuccess() = runTest {
+        coEvery { authRemoteDataSource.logout() } returns FirebaseResult.Success(Unit)
+
+        val result = authRemoteDataSource.logout()
+
+        assert(result is FirebaseResult.Success)
+
+        coVerify(exactly = 1) { authRemoteDataSource.logout() }
+
+    }
+
+    @Test
+    fun givenInvalidCredentials_whenLogout_thenFailure() = runTest {
+        coEvery { authRemoteDataSource.logout() } returns
+                FirebaseResult.UnknownError(Exception("Invalid credentials"))
+
+        val result = authRemoteDataSource.logout()
+
+        assert(result is FirebaseResult.UnknownError)
+
+        assert((result as FirebaseResult.UnknownError).exception.message == "Invalid credentials")
+
+        coVerify(exactly = 1) { authRemoteDataSource.logout() }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenLogout_thenNetworkError() = runTest {
+        coEvery { authRemoteDataSource.logout() } returns
+                FirebaseResult.FirebaseError("500", "Network error")
+
+        val result = authRemoteDataSource.logout()
+
+        assert(result is FirebaseResult.FirebaseError)
+
+        assert((result as FirebaseResult.FirebaseError).message == "Network error")
+
+        coVerify(exactly = 1) { authRemoteDataSource.logout() }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenIsUserLoggedIn_thenSuccess() = runTest {
+        coEvery { authRemoteDataSource.isUserLoggedIn() } returns true
+
+        val result = authRemoteDataSource.isUserLoggedIn()
+
+        assert(result)
+
+        coVerify(exactly = 1) { authRemoteDataSource.isUserLoggedIn() }
+
+    }
+
+    @Test
+    fun givenInvalidCredentials_whenIsUserLoggedIn_thenFailure() = runTest {
+        coEvery { authRemoteDataSource.isUserLoggedIn() } returns false
+
+        val result = authRemoteDataSource.isUserLoggedIn()
+
+        assert(!result)
+
+        coVerify(exactly = 1) { authRemoteDataSource.isUserLoggedIn() }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenGetUserId_thenSuccess() = runTest {
+        val expectedUid = "Uid123"
+
+        coEvery { authRemoteDataSource.getUserId() } returns expectedUid
+
+        val result = authRemoteDataSource.getUserId()
+
+        assert(result == expectedUid)
+
+        coVerify(exactly = 1) { authRemoteDataSource.getUserId() }
+
+    }
+
+    @Test
+    fun givenInvalidCredentials_whenGetUserId_thenFailure() = runTest {
+        coEvery { authRemoteDataSource.getUserId() } returns null
+
+        val result = authRemoteDataSource.getUserId()
+
+        assert(result == null)
+
+        coVerify(exactly = 1) { authRemoteDataSource.getUserId() }
+
+    }
+
+    @Test
+    fun givenValidCredentials_whenGetUserId_thenNetworkError() = runTest {
+        coEvery { authRemoteDataSource.getUserId() } returns null
+
+        val result = authRemoteDataSource.getUserId()
+
+        assert(result == null)
+
+        coVerify(exactly = 1) { authRemoteDataSource.getUserId() }
+
+    }
+
 }
